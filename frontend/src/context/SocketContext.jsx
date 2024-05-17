d60c2f8f0f313f16fdbf68e8a5ca3cdc46ced62d
Kiran <gkirankumar503@gmail.com>
2024-05-17 13:38:59 +0530

    Socket url updated

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -16,7 +16,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("http://localhost:5000", {
+			const socket = io("https://chit-chat-app-7osr.onrender.com/", {
 				query: {
 					userId: authUser._id,
 				},
